61b19711588441b7ffc166a6e907b9f471c0b347
tonymythical <174657016+tonymythical@users.noreply.github.com>
2026-02-19 13:19:31 -0800

    initialized pt.2

--- app.js
+++ app.js
@@ -4,5 +4,9 @@ const app = express();
 const PORT = 3002;
 
 app.get('/', (req, res) => {
-    res.send
-})
+    res.sendFile(`${import.meta.dirname}/views/home.html`);
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
